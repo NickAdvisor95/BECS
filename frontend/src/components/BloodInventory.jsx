a6f12f5805170e0e30a271f62cf932d2327946d1
alechkos <93551621+alechkos@users.noreply.github.com>
2024-09-20 02:09:44 +0300

    feat: add button to show blood inventory

---
+++ frontend/src/components/BloodInventory.jsx
@@ -0,0 +1,25 @@
+const BloodInventory = ({ title, bloodInventory }) => {
+    return (
+        <div>
+            <h2 style={{ textAlign: 'center' }}>{title}</h2> {/* Use the title prop for the h2 element */}
+            <table id="blood-inventory">
+                <thead>
+                <tr>
+                    <th>Blood Type</th>
+                    <th>Available Units</th>
+                </tr>
+                </thead>
+                <tbody>
+                {bloodInventory.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.bloodType}</td>
+                        <td>{item.amount}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default BloodInventory;
